edd990121a441f8fcb69fa60608116033dc48670
Andrei Tarasov <adnominator@bk.ru>
2022-12-05 06:49:48 +0500

    cycles2

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,62 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public long amountSales(long[] sales) {
+        long amount = 0;
+        for (long sale : sales) {
+            amount += sale;
+        }
+        return amount;
+    }
+
+    public long averageAmountSales(long[] sales) {
+        return amountSales(sales) / 12;
+    }
+
+    public int minAverageSales(long[] sales) {
+        long average = averageAmountSales(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < average){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int maxAverageSales(long[] sales) {
+        long average = averageAmountSales(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > average){
+                count++;
+            }
+        }
+        return count;
+    }
+}
